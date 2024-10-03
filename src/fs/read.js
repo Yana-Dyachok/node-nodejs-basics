a583e15e3640a2f5af20c58789fe723898584c92
Yana <annetart1994@gmail.com>
2024-10-03 22:40:59 +0300

    feat: implement function that prints content of the fileToRead.txt into console  in read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import { readFile } from 'fs/promises';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const fileName = fileURLToPath(import.meta.url);
+const dirName = dirname(fileName);
+const path = resolve(dirName, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const fileContent = await readFile(path, {
+            encoding: 'utf8',
+        });
+
+        console.log(fileContent);
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
