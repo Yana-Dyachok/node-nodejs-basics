e3aef628c332593af24072f2881022ba7bf1ea18
Yana <annetart1994@gmail.com>
2024-10-03 23:42:44 +0300

    refactor: refactor code in rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -4,14 +4,25 @@ import { fileURLToPath } from 'url';
 
 const fileName = fileURLToPath(import.meta.url);
 const dirName = dirname(fileName);
-const path = resolve(dirName, 'files', 'wrongFilename.txt');
+const wrongPath = resolve(dirName, 'files', 'wrongFilename.txt');
 const renamePath = resolve(dirName, 'files', 'properFilename.md');
+const errorMessage = 'FS operation failed';
+
+const isExists = async (path) => {
+    return fs.access(path)
+        .then(() => true)
+        .catch(() => false);
+};
 
 const rename = async () => {
     try {
-        await fs.rename(path, renamePath);
+        const isFileExists = await isExists(renamePath);
+        if (isFileExists) {
+          throw new Error(errorMessage);
+        }
+        await fs.rename(wrongPath, renamePath);
     } catch {
-        throw new Error('FS operation failed');
+        throw new Error(errorMessage);
     }
 };
 
