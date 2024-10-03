4afde0e8678378e2b4d4f6a572bb8bd49c5b0d3f
Yana <annetart1994@gmail.com>
2024-10-03 20:23:16 +0300

    feat: implement function that creates new file fresh.txt in create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { writeFile } from 'fs/promises';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const fileName = fileURLToPath(import.meta.url);
+const dirName = dirname(fileName);
+const path = resolve(dirName, 'files', 'fresh.txt');
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await writeFile(path, 'I am fresh and young', { flag: 'wx' });
+  } catch (err) {
+    if (err.code === 'EEXIST') {
+      throw new Error('FS operation failed');
+    } else throw err;
+  }
 };
 
 await create();
