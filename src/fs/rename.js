c4d5947a0a3d2a3bed9fb014166a026594679e55
Yana <annetart1994@gmail.com>
2024-10-03 23:24:22 +0300

    feat: implement function that renames file wrongFilename.txt to properFilename with extension .md in rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,18 @@
+import { promises as fs } from 'fs';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const fileName = fileURLToPath(import.meta.url);
+const dirName = dirname(fileName);
+const path = resolve(dirName, 'files', 'wrongFilename.txt');
+const renamePath = resolve(dirName, 'files', 'properFilename.md');
+
 const rename = async () => {
-    // Write your code here 
+    try {
+        await fs.rename(path, renamePath);
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await rename();
