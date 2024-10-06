653ca0e16015d5730b21091f23c4f6f94f186334
Yana <annetart1994@gmail.com>
2024-10-06 16:35:33 +0300

    feat: implement function that reads data from process.stdin, reverses text using Transform Stream and then writes it into process.stdout

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,23 @@
+import { Transform } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
+
 const transform = async () => {
-    // Write your code here 
+    process.stdout.write(
+        "Let's transform text. If you want to exit, please press Ctrl+C\nPlease enter any text and press 'Enter':\n"
+    );
+
+    const transformStream = new Transform({
+        transform(data, _, callback) {
+            const reversedData =
+                data.toString().trim().split('').reverse().join('') + '\n';
+            callback(null, reversedData);
+        },
+    });
+
+    await pipeline(process.stdin, transformStream, process.stdout);
+    process.on('SIGINT', () => {
+        process.exit();
+    });
 };
 
 await transform();
