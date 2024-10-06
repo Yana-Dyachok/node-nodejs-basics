a48baec6398aae7e500324427ffe96e2232d6eca
Yana <annetart1994@gmail.com>
2024-10-06 15:56:49 +0300

    feat: implement function that writes data using Writable Stream and  prints it's using process.stdin

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,34 @@
+import { createWriteStream } from 'node:fs';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const fileName = fileURLToPath(import.meta.url);
+const dirName = dirname(fileName);
+const path = resolve(dirName, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    try {
+        process.stdout.write(
+            "Let's create a file. If you want to exit, please enter 'exit' or press Ctrl+C\nPlease enter any text:\n"
+        );
+        const output = createWriteStream(path, { flags: 'w' });
+
+        process.stdin.on('data', (chunk) => {
+            if (chunk.toString().trim() === 'exit') {
+                output.end();
+                process.exit();
+            } else {
+                output.write(chunk);
+            }
+        });
+
+        process.on('SIGINT', () => {
+            output.end();
+            process.exit();
+        });
+    } catch (error) {
+        console.error('Error writing:', error);
+    }
 };
 
 await write();
